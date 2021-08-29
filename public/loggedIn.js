fde4b6ce010e9b1d0287be93b4f3a580c8d9302b
edelamaza <edelamaza@me.com>
2021-08-29 07:28:45 -0500

    added mails

--- public/loggedIn.js
+++ public/loggedIn.js
@@ -2,6 +2,13 @@ var h2 = document.getElementById('deviceText');
 // let todaysDate = new Date().toLocaleDateString('es-MX').replaceAll('/','');
 h2.innerHTML = `You Scanned Device: ${localStorage.deviceid}`;
 let hashKey;
+let arrHashes = [];
+let emailArray = [];
+
+firebase.database().ref('pairs/' + localStorage.deviceid).on('value', function (snapshot) {
+    hashKey = snapshot.val()?.hash;
+    console.log(snapshot.val()?.hash)
+});
 
 const btnUnlink = document.getElementById("unlink");
 const btnLink = document.getElementById("link");
@@ -13,7 +20,8 @@ function writeUserDataLink(uid, deviceID, email) {
         deviceID: deviceID,
         date: new Date().toLocaleString('es-MX', { timeZone: 'America/Monterrey' }),
         dateEnd: 0,
-        email: email
+        email: email,
+        contacts: ['']
     }).key;
 
     firebase.database().ref(`pairs/${deviceID}`).set({ // estaba en set
@@ -60,10 +68,37 @@ btnSos.addEventListener("click", (e) => {
     console.log('Jalo')
     e.preventDefault();
 
-    arrHashes = [];
     //location.href = "loggedOut.html"
-    firebase.database().ref('persons/').on('value', function(snapshot){
-        arrHashes.push()
+    let counter = 0;
+    let keys;
+    firebase.database().ref().child('persons/').on('value', function(snapshot){
+        keys = Object.keys(snapshot.val());
+        console.log(snapshot.val())
+        var childKey = snapshot.child(`persons/${keys[0]}`).key;
+        console.log(childKey)
+        console.log(snapshot.val()[`${childKey}`].uid)
+        console.log(localStorage.uid)
+        for (let i = 0; i < keys.length; i++) {
+            var childKey = snapshot.child(`persons/${keys[i]}`).key;
+            if (snapshot.val()[`${childKey}`].uid === localStorage.uid) {
+                console.log('Hello w')
+                
+                propertyNames = Object.values(snapshot.val()[`${childKey}`].contacts);
+                console.log(propertyNames)
+                arrHashes.push(propertyNames)
+            }
+        }
+        
+        emailArray = arrHashes.flat();
+        console.log(emailArray)
+
+        // const arrContacts = snapshot.val().map((item) => {
+        //     if (typeof item === 'object') {
+        //         return item
+        //     }
+        // })
     });
 });
+
+console.log(emailArray);
 
